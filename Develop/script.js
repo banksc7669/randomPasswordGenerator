917521c8b81587a87976f5ab8d2c4963eec65ba2
CBanks <banksc7669@yahoo.com>
2020-09-22 13:04:24 -0400

    variables added as place holders for types of characters

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+var char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var num = '0123456789';
+var sym = '!@#$%^&*=-_';
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
