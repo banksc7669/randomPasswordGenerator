5e444ecee67f7cfd7c3afee994963607c2f95f76
CBanks <banksc7669@yahoo.com>
2020-09-22 16:30:32 -0400

    javascript and HTML files updated

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,26 @@
+// generate random password
+function writePassword (){
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("Generate Password").value;
 
 // Write password to the #password input
-var char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var num = '0123456789';
-var sym = '!@#$%^&*=-_';
+var values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*=-_";
+var password = "";
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//create for loop to choose password characters
+for(var i = 0; i <= generateBtn; i++){
+  password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+}
+return password;
 
-  passwordText.value = password;
+// add password to display area
+var password = writePassword();
+document.getElementById("#wrapper").value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
