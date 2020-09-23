53a0f045f10aecbb3ed7762ec5307adec199c9ee
CBanks <banksc7669@yahoo.com>
2020-09-22 21:36:45 -0400

    add event listner

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,10 @@ function writePassword(){
   var values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*=-_";
   var password = "";
   
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+
+  
   //create for loop to choose password characters
   for(var i = 0; i <= generateBtn; i++){
     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
